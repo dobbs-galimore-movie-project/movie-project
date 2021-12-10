5cf635e9e5e5f91d401b01ae9e7a4f995bf86a98
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-10 13:44:04 -0600

    linked js and css files to HTML

---
+++ js/movie-project.js
@@ -0,0 +1,2 @@
+//TODO: Make an AJAX request to get a listing of all the movies
+//TODO: When the initial AJAX request comes back, remove the "loading..." message and replace it with HTML generated from the json response your code receives
